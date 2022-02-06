160732bc7874c5bac2ade6884a240008a505d665
Sangpham02042001 <psang242001@gmail.com>
2022-02-06 08:10:33 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+const HOST = process.env.HOST || "localhost";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(PORT, HOST, () => {
+  console.log(`server is running on port ${HOST}:${PORT}`);
+});
