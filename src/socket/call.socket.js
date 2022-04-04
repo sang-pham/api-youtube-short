f16725a7045fd03189c0e97c7302065df1d5c609
Vdtrung_ <hellovdtvip@gmail.com>
2022-04-05 02:30:14 +0700

    update video call

--- src/socket/call.socket.js
+++ src/socket/call.socket.js
@@ -1,14 +1,18 @@
 const { emitToMany } = require("../lib");
 
 const callSocket = (io, socket, userSockets) => {
-  socket.on("video-call-start", ({ senderId, receiverId, chatBoxId }) => {
-    emitToMany(socket, userSockets[receiverId], "video-call-start", {
-      senderId,
-      receiverId,
-      chatBoxId,
-    });
-    console.log("call start");
-  });
+  socket.on(
+    "video-call-start",
+    ({ senderId, receiverId, chatBoxId, offer }) => {
+      emitToMany(socket, userSockets[receiverId], "video-call-start", {
+        senderId,
+        receiverId,
+        chatBoxId,
+        offer,
+      });
+      console.log("call start");
+    }
+  );
 
   socket.on("video-call-stop", ({ senderId, receiverId, chatBoxId }) => {
     emitToMany(socket, userSockets[receiverId], "video-call-stop", {
@@ -19,20 +23,11 @@ const callSocket = (io, socket, userSockets) => {
     console.log("call stop");
   });
 
-  socket.on("video-call-offer", ({ senderId, receiverId, payload }) => {
-    emitToMany(socket, userSockets[receiverId], "video-call-offer", {
-      senderId,
-      receiverId,
-      sdp: payload,
-    });
-    console.log(payload, receiverId);
-  });
-
   socket.on("video-call-answer", ({ senderId, receiverId, payload }) => {
     emitToMany(socket, userSockets[receiverId], "video-call-answer", {
       senderId,
       receiverId,
-      sdp: payload,
+      answer: payload,
     });
   });
 
